2bcbbd928cea50a4e7f75f1d087e27c206cd011f
Calvin Baldwin <baldwicl@gmail.com>
2026-02-13 22:59:49 -0500

    Hide leaderboard tab - admin only access via direct URL

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,9 +3,9 @@ import { Tabs } from 'expo-router';
 export default function TabLayout() {
   return (
     <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="index" options={{ title: 'Game' }} />
-      <Tabs.Screen name="leaderboard" options={{ title: 'Leaderboard' }} />
-      <Tabs.Screen name="about" options={{ title: 'About' }} />
+      <Tabs.Screen name="index" options={{ title: 'Game', href: '/' }} />
+      <Tabs.Screen name="leaderboard" options={{ title: 'Leaderboard', href: null }} />
+      <Tabs.Screen name="about" options={{ title: 'About', href: null }} />
     </Tabs>
   );
 }
